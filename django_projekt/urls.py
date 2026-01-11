22db889c21453213b2d2c94c5dddd1368aa7b43a
dnadler121 <dnadler121@gmail.com>
2026-01-11 21:47:26 +0100

    Update urls.py

--- django_projekt/urls.py
+++ django_projekt/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import include,path
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Aplikace bezi ✅")
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path("", home),
-    path('admin/', admin.site.urls),
-    path('',include('django_aplikace.urls'))
+    path("admin/", admin.site.urls),
+    path("", include("django_aplikace.urls")),
 ]
+
+
 
